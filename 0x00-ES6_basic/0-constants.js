01853c19d111c5c2981621a0cdefc5223d6e9072
patoctd4real <patoctd4real@yahoo.com>
2023-04-17 22:06:35 +0000

    node

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+export function startChore() {
+  const chore = 'I prefer const when I can.';
+  return chore;
+}
+
+export function endMost() {
+  return ' is okay';
+}
+
+export function upcomingTask() {
+  let param = 'But sometimes let';
+  param += endMost();
+
+  return param;
+}
