811892068d50aa77e038d57a9ad88da8870c9f22
patoctd4real <patoctd4real@yahoo.com>
2023-04-18 01:39:30 +0000

    updated

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,20 @@
 export default function createIteratorObject(report) {
-  const employees = [];
-  for (const [department, departmentEmployees] of Object.entries(report.allEmployees)) {
-    employees.push(...departmentEmployees);
-  }
-  return employees;
+  const all = Object.values(report.allEmployees).reduce((a, b) => {
+    a.push(...b);
+    return a;
+  }, []);
+  let currIndex = 0;
+  const maxIndex = all.length;
+  return {
+    next() {
+      if (currIndex < maxIndex) {
+        const result = { value: all[currIndex], done: false };
+        currIndex += 1;
+        return result;
+      }
+      return { value: null, done: true };
+    },
+    [Symbol.iterator]: () => this.next(),
+  };
 }
 
