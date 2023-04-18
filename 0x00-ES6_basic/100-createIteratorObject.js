8891585e4bd75e7ed0cfe35cde85af9ab004c365
patoctd4real <patoctd4real@yahoo.com>
2023-04-18 01:57:29 +0000

    updat

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,20 +1,29 @@
 export default function createIteratorObject(report) {
-  const all = Object.values(report.allEmployees).reduce((a, b) => {
-    a.push(...b);
-    return a;
-  }, []);
-  let currIndex = 0;
-  const maxIndex = all.length;
+  let currentDeptIndex = 0;
+  let currentEmployeeIndex = 0;
+  const departments = Object.values(report.allEmployees);
+
   return {
     next() {
-      if (currIndex < maxIndex) {
-        const result = { value: all[currIndex], done: false };
-        currIndex += 1;
-        return result;
+      if (currentDeptIndex >= departments.length) {
+        // No more departments to iterate through
+        return { done: true };
       }
-      return { value: null, done: true };
+
+      const currentDept = departments[currentDeptIndex];
+      if (currentEmployeeIndex >= currentDept.length) {
+        // Move on to the next department
+        currentDeptIndex++;
+        currentEmployeeIndex = 0;
+        return this.next(); // Recursive call to move to the next employee
+      }
+
+      // Return the next employee in the current department
+      const nextEmployee = currentDept[currentEmployeeIndex];
+      currentEmployeeIndex++;
+      return { value: nextEmployee, done: false };
     },
-    [Symbol.iterator]: () => this.next(),
   };
 }
+
 
