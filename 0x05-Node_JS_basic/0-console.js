a03ead2ce9794d0e2110560ff05d5257c08f8b8b
patoctd4real <patoctd4real@yahoo.com>
2023-06-17 04:44:43 +0000

    update

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,5 @@
-/**
- * Display a message on the STDOUT console
- * @param {string} message - The message to display
- * @author Patirck christopher<https://github.com?patoctd4real>
- */
-const displayMessage = (ms) => {
-    console.log(ms)
-};
+function displayMessage(data) {
+  console.log(data);
+}
 
 module.exports = displayMessage;
